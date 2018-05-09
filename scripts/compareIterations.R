30dcf13f43decf36d58bb6f1fe0215a3a5060cbb
Adon Rosen <adrose@galton.uphs.upenn.edu>
2018-05-08 21:41:28 -0400

    More aggressive cpu allocation

--- scripts/compareIterations.R
+++ scripts/compareIterations.R
@@ -11,7 +11,8 @@ dmin <- c(-2, -1, 0, 1)
 dmin1 <- c(-2, -1, 0, 1)
 all.folds <- expand.grid(probes,amin,dmin,dmin1)
 
-
+cl <- makeCluster(28)
+registerDoParallel(cl)
 for(i in 1:dim(all.folds)[1]){
     nsim <- 2000      # how many simulations?
     ne <- 1000        # how many examinees per simulation?
@@ -27,33 +28,29 @@ for(i in 1:dim(all.folds)[1]){
     cor_crit <- 0.8   # true correlation between criterion and theta
     
     v <- sqrt((1/cor_crit^2)-1)  # convert cor_crit to variance of residuals
-    cl <- makeCluster(8)
-    registerDoParallel(cl)
     results <- foreach(z=seq(1,nsim), .combine=rbind) %dopar% {
         library(psych)
         a <- runif(nitems,amin,amax)                                         # set item discriminations
         d <- c(runif(probes,dminp,dmaxp),runif((nitems-probes),dmin,dmax))   # set item difficulties
         dat <- sim.irt(nvar = nitems, n = ne, a=a, d=d)                      # simulate data
         try(modp <- irt.fa(dat$items,plot=FALSE))                            # calibrate item parameters before applying skip logic
-        try(IRT_scores_no_SL <- scoreIrt(modp,dat$items)$theta1)             # calculate IRT scores before applying skip logic
+        try(IRT_scores_no_SL <- score.irt(modp,dat$items)$theta1)             # calculate IRT scores before applying skip logic
         sum_no_SL <- rowSums(dat$items)                                      # calculate sum scores before applying skip logic
         for (i in 1:dim(dat$items)[1]) {if (rowSums(dat$items[,1:probes])[i] == 0) {dat$items[i,(probes+1):nitems] <- 0}}   # apply the skip logic
         try(mod <- irt.fa(dat$items,plot=FALSE))                             # calibrate item parameters after applying skip logic
         DV <- dat$theta + rnorm(ne,0,v)                                      # create criterion variable (DV to be predicted by scores)
-        try(IRT_scores <- scoreIrt(mod,dat$items)$theta1)                    # calculate IRT scores after applying skip logic
+        try(IRT_scores <- score.irt(mod,dat$items)$theta1)                    # calculate IRT scores after applying skip logic
         sum_scores <- rowSums(dat$items)                                     # calculate sum scores after applying skip logic
         x <- data.frame(DV,IRT_scores_no_SL,sum_no_SL,IRT_scores,sum_scores)
         cor(x)[2:5,1]
     }
-    # Kill the cluster
-    stopCluster(cl)
     
     # Now create the DV variable
     a <- runif(nitems,amin,amax)                                         # set item discriminations
     d <- c(runif(probes,dminp,dmaxp),runif((nitems-probes),dmin,dmax))   # set item difficulties
     dat <- sim.irt(nvar = nitems, n = ne, a=a, d=d)                      # simulate data
     try(modp <- irt.fa(dat$items,plot=FALSE))                            # calibrate item parameters before applying skip logic
-    try(IRT_scores_no_SL <- scoreIrt(modp,dat$items)$theta1)             # calculate IRT scores before applying skip logic
+    try(IRT_scores_no_SL <- score.irt(modp,dat$items)$theta1)             # calculate IRT scores before applying skip logic
     sum_no_SL <- rowSums(dat$items)                                      # calculate sum scores before applying skip logic
     for (i in 1:dim(dat$items)[1]) {if (rowSums(dat$items[,1:probes])[i] == 0) {dat$items[i,(probes+1):nitems] <- 0}}   # apply the skip logic
     try(mod <- irt.fa(dat$items,plot=FALSE))                             # calibrate item parameters after applying skip logic
@@ -64,7 +61,8 @@ for(i in 1:dim(all.folds)[1]){
     
     write.csv(results,paste("sims",nsim,ne,nitems,probes,amin,amax,dminp,dmaxp,dmin,dmax,"final.csv", sep='_'))
 }
-
+# Kill the cluster
+stopCluster(cl)
 # Now create a plot with the average corellation values from each of these output csv's
 outCorVals <- NULL
 for(i in 1:dim(all.folds)[1]){
